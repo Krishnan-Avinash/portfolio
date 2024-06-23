15dc545989102511e39ba78156105ae1de79b662
Avinash <avinash.krishnan1605@gmail.com>
2024-06-23 15:39:05 +0530

    Connected contact page to backend

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import call from "../../assets/callus2png.png";
 import mail from "../../assets/mail2png.png";
 
+import axios from "axios";
+
+import { useToast } from "@chakra-ui/react";
+
 const Contact = () => {
+  const toast = useToast();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [num, setNum] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = {
+      name: name,
+      email: email,
+      number: num,
+      message: message,
+    };
+
+    const response = await axios.post(
+      "https://portfolio-backend-g6av.onrender.com/api/portfolio/add",
+      formData
+    );
+    if (response.data.success) {
+      toast({
+        title: "Successful..!!",
+        status: "success",
+        duration: 5000,
+        isClosable: false,
+      });
+    } else {
+      toast({
+        title: "Some Error Occured.",
+        status: "error",
+        duration: 5000,
+        isClosable: false,
+      });
+    }
+  };
+
   return (
     <>
       <div className="contact-container">
@@ -27,24 +67,40 @@ const Contact = () => {
               <p> avinash.krishnan1605@gmail.com</p>
             </section>
           </div>
-          <div className="contact-right">
+          <form className="contact-right" onSubmit={onSubmitHandler}>
             <div className="contact-right-top">
-              <input type="text" placeholder="Your Name" required />
-              <input type="email" placeholder="Your Email" required />
-              <input type="number" placeholder="Your Phone" required />
+              <input
+                type="text"
+                placeholder="Your Name"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <input
+                type="email"
+                placeholder="Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                type="number"
+                placeholder="Your Phone"
+                onChange={(e) => setNum(e.target.value)}
+                required
+              />
             </div>
             <div className="contact-right-mid">
               <textarea
                 name="message"
                 id="message"
                 placeholder="Your message"
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
               {/* <input type="text" placeholder="Your Message" required /> */}
             </div>
             <div className="contact-right-bottom">
-              <button>Send Message</button>
+              <button type="submit">Send Message</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
