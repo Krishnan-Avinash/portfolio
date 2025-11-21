5352aa3c65ce37247e35dbc9bff23261370fbc1d
Avinash <avinash.krishnan1605@gmail.com>
2025-11-21 20:34:07 +0530

    Changed project pic

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import proj1 from "../../assets/project1Img.jpg";
-import proj2 from "../../assets/proj21.jpg";
-import proj3 from "../../assets/proj33.jpg";
-import proj1Vid from "../../assets/proj1Trim2.mov";
-import proj2Vid from "../../assets/proj2Trim.mp4";
-import proj3Vid from "../../assets/proj3Vid.mp4";
+import proj3 from "../../assets/project1Img.jpg";
+import proj1 from "../../assets/proj21.jpg";
+import proj2 from "../../assets/proj22.jpg";
+import proj3Vid from "../../assets/proj1Trim2.mov";
+import proj1Vid from "../../assets/proj2Trim.mp4";
+import proj2Vid from "../../assets/proj3Vid.mp4";
 import IndividualProject from "./IndividualProject";
 import { Element } from "react-scroll";
 
 const Projects = () => {
   const proj = [
     {
-      img: proj2,
-      vid: proj2Vid,
+      img: proj1,
+      vid: proj1Vid,
       title: "OneClickMart",
       desc: "OneClickMart, a highly responsive, user-friendly e-commerce platform using React and Chakra UI, featuring dynamic shopping functionalities.",
       liveLink: "https://oneclickmart.netlify.app/",
@@ -20,8 +20,8 @@ const Projects = () => {
       id: 1,
     },
     {
-      img: proj3,
-      vid: proj3Vid,
+      img: proj2,
+      vid: proj2Vid,
       title: "Code Reviewer",
       desc: "Code Reviewer is a powerful VS Code extension that leverages the Google Gemini API to provide comprehensive code reviews. Enhance your coding workflow with AI-driven insights, automated feedback, and suggestions for improvements. Perfect for developers seeking to elevate their code quality effortlessly.",
       liveLink:
@@ -30,8 +30,8 @@ const Projects = () => {
       id: 2,
     },
     {
-      img: proj1,
-      vid: proj1Vid,
+      img: proj3,
+      vid: proj3Vid,
       title: "UsabilityHub",
       desc: "Usabilityhub is an online platform whose frontend is cloned from their original webdesign and a new backend is developed  for users to upload and share website templates.  Need assistance? Our integrated chatbox powered by emailJS is there to help.",
       liveLink: "https://designusability.netlify.app/",
